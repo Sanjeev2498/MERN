9636cb6dfcb7e971ab24cb3b42f0fb7b43bceaa6
Sanjeev <sanjeevsangwan0999@gmail.com>
2025-11-17 13:26:45 +0530

    connecting to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("Server is connected to Database Successfully");
+})
+.catch((err)=>{
+console.log(`Error on Site`,err);
 
+});
 const app=express();
 
 app.listen(9000,()=>{
